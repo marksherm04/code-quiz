67bdf0111dd9a20db779b145bbf70a08ffa68f5c
Mark Sherman <marksherman@Marks-iMac.local>
2021-05-11 16:57:51 -0400

    set up section for question and answered, styled the box in css and added buttons

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// 1. You will have <span>60 seconds</span> to complete the quiz.
+// 2. If you get the question wrong, it will deduct 10 seconds.
+// 3. Your score will be based on how much time is left in the quiz.
+// 4. High score is listed at top left of the page.
+// 5. Good luck!
+
+// What does HTML stand for?
+// HyperText Preprocessor
+// HyperText Markup Language
+// HyperText Multiple Language
+// HyperTool Multi Language
+
+// 
+// 
+// 
+//
+// 
+
+// 
+// 
+// 
+// 
+//
+
+// 
+// 
+// 
+// 
+// 
+
+// 
+// 
+// 
+// 
+// 
